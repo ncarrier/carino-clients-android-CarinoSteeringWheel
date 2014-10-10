30ceeb2b5632631e6239e7976768ebcc59f1415d
Nicolas CARRIER <carrier.nicolas0@gmail.com>
2014-10-10 21:56:49 +0200

    DA BIG COMMMMIIIIIITTTTTTTT

--- src/org/nicar/carinosteeringwheel/AccelerometerListener.java
+++ src/org/nicar/carinosteeringwheel/AccelerometerListener.java
@@ -3,34 +3,60 @@ package org.nicar.carinosteeringwheel;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
 import android.widget.ProgressBar;
 
-public class AccelerometerListener implements SensorEventListener {
-	private static final String TAG = SteeringWheel.TAG;
-
+public class AccelerometerListener extends Thread implements
+		SensorEventListener {
 	private float gravity[];
 	private CarinoListener carinoSelector;
 	private ProgressBar bar1;
 	private ProgressBar bar2;
 	private ProgressBar bar3;
 
+	private final static double UI_REFRESH_TIME = 50.;
+	public static final String TAG = "CarinoSteeringWheel";
+
+	private long previous_ui_refresh_timestamp = 0;
+
+	private SensorManager mSensorManager;
+
+	private Sensor mAccelerometer;
+
+	private Object mLock;
+	private boolean mRegister;
+	private boolean mRunning;
+
 	public AccelerometerListener(CarinoListener carinoSelector,
-			ProgressBar bar1, ProgressBar bar2, ProgressBar bar3) {
+			ProgressBar bar1, ProgressBar bar2, ProgressBar bar3,
+			SensorManager sensorManager) {
+		super();
+
 		this.carinoSelector = carinoSelector;
 		this.bar1 = bar1;
 		this.bar2 = bar2;
 		this.bar3 = bar3;
 		gravity = new float[3];
+		mSensorManager = sensorManager;
+		mAccelerometer = mSensorManager
+				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		mLock = new Object();
+		this.mRegister = false;
+		this.mRunning = true;
 	}
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
 		float alpha = (float) 0.8;
 		float norm;
-		byte x;
-		byte y;
-		byte z;
+		int x;
+		int y;
+		int z;
 		String msgString;
+		long ts;
+
+		ts = System.currentTimeMillis();
 
 		/* lowpass filter the gravity vector to filter.udden movements */
 		gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
@@ -43,13 +69,18 @@ public class AccelerometerListener implements SensorEventListener {
 		 */
 		norm = (float) Math.sqrt(Math.pow(gravity[0], 2)
 				+ Math.pow(gravity[1], 2) + Math.pow(gravity[2], 2));
-		x = (byte) (50 * (gravity[0] / norm) + 50);
-		y = (byte) (50 * (gravity[1] / norm) + 50);
-		z = (byte) (50 * (gravity[2] / norm) + 50);
+		double max = bar1.getMax() / 2;
+		double k = max / norm;
+		x = (int) (k * gravity[0] + max);
+		y = (int) (k * gravity[1] + max);
+		z = (int) (k * gravity[2] + max);
 
-		bar1.setProgress((int) x);
-		bar2.setProgress((int) y);
-		bar3.setProgress((int) z);
+		if (ts - previous_ui_refresh_timestamp > UI_REFRESH_TIME) {
+			previous_ui_refresh_timestamp = ts;
+			bar1.setProgress(x);
+			bar2.setProgress(y);
+			bar3.setProgress(z);
+		}
 
 		msgString = "x = " + x + ", y = " + y + ", z = " + z + "\n";
 		this.carinoSelector.postMessage(msgString.getBytes());
@@ -60,4 +91,46 @@ public class AccelerometerListener implements SensorEventListener {
 		/* nothing to do */
 	}
 
+	@Override
+	public void run() { 
+		synchronized (mLock) {
+			do {
+				if (this.mRegister) {
+					Log.d(TAG, "register");
+					mSensorManager.registerListener(this, mAccelerometer,
+							SensorManager.SENSOR_DELAY_UI);
+				} else {
+					Log.d(TAG, "unregister");
+					mSensorManager.unregisterListener(this);
+				}
+				try {
+					this.mLock.wait();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} while (this.mRunning);
+		}
+	}
+
+	public void register() {
+		synchronized (mLock) {
+			this.mRegister = true;
+			mLock.notify();
+		}
+	}
+
+	public void unregister() {
+		synchronized (mLock) {
+			this.mRegister = false;
+			mLock.notify();
+		}
+	}
+
+	public void end() {
+		synchronized (mLock) {
+			this.mRunning = false;
+			mLock.notify();
+		}
+	}
 }
--- src/org/nicar/carinosteeringwheel/SteeringWheel.java
+++ src/org/nicar/carinosteeringwheel/SteeringWheel.java
@@ -1,87 +1,21 @@
 package org.nicar.carinosteeringwheel;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-import org.nicar.carinosteeringwheel.util.SystemUiHider;
-
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.Context;
-import android.hardware.Sensor;
 import android.hardware.SensorManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ProgressBar;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- * 
- * @see SystemUiHider
- */
 public class SteeringWheel extends Activity {
 	public static final String TAG = "CarinoSteeringWheel";
 
-	/**
-	 * Whether or not the system UI should be auto-hidden after
-	 * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-	 */
-	private static final boolean AUTO_HIDE = true;
-
-	/**
-	 * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-	 * user interaction before hiding the system UI.
-	 */
-	private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-	/**
-	 * If set, will toggle the system UI visibility upon interaction. Otherwise,
-	 * will show the system UI visibility upon interaction.
-	 */
-	private static final boolean TOGGLE_ON_CLICK = true;
-
-	/**
-	 * The flags to pass to {@link SystemUiHider#getInstance}.
-	 */
-	private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
-
-	/**
-	 * The instance of the {@link SystemUiHider} for this activity.
-	 */
-	private SystemUiHider mSystemUiHider;
-
-	private Sensor mAccelerometer;
-	private SensorManager mSensorManager;
-
 	private CarinoListener carinoSelector;
 	private AccelerometerListener accelerometerListener;
+	private static final int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+			| View.SYSTEM_UI_FLAG_FULLSCREEN;
+	private View decorView;
 
-	protected static String readGateway() throws IOException {
-		Process process = new ProcessBuilder().command("/system/bin/getprop")
-				.redirectErrorStream(true).start();
-		try {
-			BufferedReader in = new BufferedReader(new InputStreamReader(
-					process.getInputStream()));
-
-			String line;
-			while ((line = in.readLine()) != null) {
-				String property[] = line.replaceAll("\\Q[\\E|\\Q]\\E", "")
-						.split(":");
-				if (property[0].matches("net\\Q.\\E.*\\Q.\\Egw"))
-					return property[1];
-			}
-		} finally {
-			process.destroy();
-		}
-
-		return null;
-	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -91,147 +25,32 @@ public class SteeringWheel extends Activity {
 
 		setContentView(R.layout.activity_steering_wheel);
 
-		final View controlsView = findViewById(R.id.fullscreen_content_controls);
+		decorView = getWindow().getDecorView();
 
-		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-		mAccelerometer = mSensorManager
-				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 		carinoSelector = new CarinoListener();
 		carinoSelector.start();
+		carinoSelector.setPriority(Thread.NORM_PRIORITY - 1);
 		accelerometerListener = new AccelerometerListener(carinoSelector,
 				(ProgressBar) findViewById(R.id.progressBar1),
 				(ProgressBar) findViewById(R.id.progressBar2),
-				(ProgressBar) findViewById(R.id.progressBar3));
-
-		/*
-		 * TODO if the car is the access point, we could use the gateway as the
-		 * car's address -> rather use DHCP information
-		 */
-		try {
-			Log.e(TAG, "gateway is " + SteeringWheel.readGateway());
-		} catch (IOException e3) {
-			// TODO Auto-generated catch block
-			e3.printStackTrace();
-		}
-
-		// Set up an instance of SystemUiHider to control the system UI for
-		// this activity.
-		mSystemUiHider = SystemUiHider.getInstance(this, controlsView,
-				HIDER_FLAGS);
-		mSystemUiHider.setup();
-		mSystemUiHider
-				.setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
-					// Cached values.
-					int mControlsHeight;
-					int mShortAnimTime;
-
-					@Override
-					@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-					public void onVisibilityChange(boolean visible) {
-						if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-							// If the ViewPropertyAnimator API is available
-							// (Honeycomb MR2 and later), use it to animate the
-							// in-layout UI controls at the bottom of the
-							// screen.
-							if (mControlsHeight == 0) {
-								mControlsHeight = controlsView.getHeight();
-							}
-							if (mShortAnimTime == 0) {
-								mShortAnimTime = getResources().getInteger(
-										android.R.integer.config_shortAnimTime);
-							}
-							controlsView
-									.animate()
-									.translationY(visible ? 0 : mControlsHeight)
-									.setDuration(mShortAnimTime);
-						} else {
-							// If the ViewPropertyAnimator APIs aren't
-							// available, simply show or hide the in-layout UI
-							// controls.
-							controlsView.setVisibility(visible ? View.VISIBLE
-									: View.GONE);
-						}
-
-						if (visible && AUTO_HIDE) {
-							// Schedule a hide().
-							delayedHide(AUTO_HIDE_DELAY_MILLIS);
-						}
-					}
-				});
-
-		// Set up the user interaction to manually show or hide the system UI.
-		controlsView.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				if (TOGGLE_ON_CLICK) {
-					mSystemUiHider.toggle();
-				} else {
-					mSystemUiHider.show();
-				}
-			}
-		});
-
-		// Upon interacting with UI controls, delay any scheduled hide()
-		// operations to prevent the jarring behavior of controls going away
-		// while interacting with the UI.
-		findViewById(R.id.dummy_button).setOnTouchListener(
-				mDelayHideTouchListener);
+				(ProgressBar) findViewById(R.id.progressBar3),
+				(SensorManager) getSystemService(SENSOR_SERVICE));
+		accelerometerListener.setPriority(Thread.NORM_PRIORITY - 1);
+		accelerometerListener.start();
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
 
-		mSensorManager.registerListener(accelerometerListener, mAccelerometer,
-				SensorManager.SENSOR_DELAY_GAME);
+		decorView.setSystemUiVisibility(uiOptions);
+		accelerometerListener.register();
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
 
-		mSensorManager.unregisterListener(accelerometerListener);
-	}
-
-	@Override
-	protected void onPostCreate(Bundle savedInstanceState) {
-		super.onPostCreate(savedInstanceState);
-
-		// Trigger the initial hide() shortly after the activity has been
-		// created, to briefly hint to the user that UI controls
-		// are available.
-		delayedHide(100);
-	}
-
-	/**
-	 * Touch listener to use for in-layout UI controls to delay hiding the
-	 * system UI. This is to prevent the jarring behavior of controls going away
-	 * while interacting with activity UI.
-	 */
-	View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-		@Override
-		public boolean onTouch(View view, MotionEvent motionEvent) {
-			if (AUTO_HIDE) {
-				delayedHide(AUTO_HIDE_DELAY_MILLIS);
-			}
-			return false;
-		}
-	};
-
-	Handler mHideHandler = new Handler();
-	Runnable mHideRunnable = new Runnable() {
-		@Override
-		public void run() {
-			mSystemUiHider.hide();
-		}
-	};
-
-	/**
-	 * Schedules a call to hide() in [delay] milliseconds, canceling any
-	 * previously scheduled calls.
-	 */
-	private void delayedHide(int delayMillis) {
-		mHideHandler.removeCallbacks(mHideRunnable);
-		mHideHandler.postDelayed(mHideRunnable, delayMillis);
+		accelerometerListener.unregister();
 	}
 }
